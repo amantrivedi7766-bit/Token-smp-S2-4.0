976605c6a538f8af24e2773d1040618af94c0a4f
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-13 21:28:19 +0530

    Implement TokenCommand for player token GUI access

---
+++ src/main/java/com/tokensmp/commands/TokenCommand.java
@@ -0,0 +1,20 @@
+package com.tokensmp.commands;
+
+import com.tokensmp.TokenSMPPlugin;
+import com.tokensmp.gui.PlayerTokensGUI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TokenCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+        new PlayerTokensGUI().open(p);
+        return true;
+    }
+}
