2ad82188f6da186266e8b7b32735c4cacb13ac84
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-13 21:32:53 +0530

    Add Ability class for player actions

---
+++ src/main/java/com/tokensmp/token/Ability.java
@@ -0,0 +1,23 @@
+package com.tokensmp.token;
+
+import org.bukkit.entity.Player;
+import java.util.function.Consumer;
+
+public class Ability {
+    private final String name;
+    private final String description;
+    private final int cooldownSeconds;
+    private final Consumer<Player> action;
+
+    public Ability(String name, String description, int cooldownSeconds, Consumer<Player> action) {
+        this.name = name;
+        this.description = description;
+        this.cooldownSeconds = cooldownSeconds;
+        this.action = action;
+    }
+
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public int getCooldownSeconds() { return cooldownSeconds; }
+    public void execute(Player player) { action.accept(player); }
+}
